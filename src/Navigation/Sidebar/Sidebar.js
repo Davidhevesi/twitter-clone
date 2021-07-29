1351f5ba0f99766e6cb79ff57023d85ac1862f6c
David Hevesi <davidhevesi2008@gmail.com>
2021-07-29 12:17:33 -0400

    Create navigation folders

---
+++ src/Navigation/Sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Twitter } from '@material-ui/icons'
+function Sidebar() {
+    return (
+        <div>
+            <Twitter />
+        </div>
+    )
+}
+
+export default Sidebar
+
